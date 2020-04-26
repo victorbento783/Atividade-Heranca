7c3dc0c8e9b3bc8c878f984acd09878bab507959
Victor Bento <61336332+victorbento783@users.noreply.github.com>
2020-04-26 04:04:14 -0300

    Add files via upload

---
+++ Atividade Herença/Atividade Herença/Operario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_Herença
+{
+    class Operario : Colaborador
+    {
+        private double valorProducao;
+        private int comissao;
+
+        public Operario(string Nome, string End, string Tel) : base(Nome, End, Tel)
+        { }
+
+        public double ValorProducao
+        {
+            get { return valorProducao; }
+            set { valorProducao = value; }
+        }
+
+        public int Comissao
+        {
+            get { return comissao; }
+            set { comissao = value; }
+        }
+
+        public override double Salario()
+        {
+            double valorComissao = valorProducao - (valorProducao * ((double)Comissao / 100));
+            return base.Salario() + valorComissao;
+        }
+    }
+}
---
+++ Atividade Herença/Atividade Herença/Pessoa.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_Herença
+{
+    class Pessoa
+    {
+        private string nome;
+        private string endereco;
+        private string telefone;
+
+        public Pessoa(string NOME, string ENDERECO, string TELEFONE)
+        {
+            this.nome = NOME;
+            this.endereco = ENDERECO;
+            this.telefone = TELEFONE;
+
+        }
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        public string Endereco
+        {
+            get { return endereco; }
+            set { endereco = value; }
+
+        }
+        public string Telefone
+        {
+            get { return telefone; }
+            set { telefone = value; }
+        }
+    }
+}
